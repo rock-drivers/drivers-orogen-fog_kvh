9795868c015ba497efe732ee73a0a235b622ea11
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2011-10-19 14:05:13 +0200

    added test-script

---
+++ scripts/test.rb
@@ -0,0 +1,30 @@
+require 'orocos'
+include Orocos
+
+if !ARGV[0]
+    STDERR.puts "usage: test.rb <device name>"
+    exit 1
+end
+
+ENV['PKG_CONFIG_PATH'] = "#{File.expand_path("..", File.dirname(__FILE__))}/build:#{ENV['PKG_CONFIG_PATH']}"
+
+Orocos.initialize
+
+Orocos::Process.spawn 'dsp3000' do |p|
+    driver = p.task 'dsp3000'
+    Orocos.log_all_ports
+
+    driver.port = ARGV[0]
+    driver.configure
+    driver.start
+   
+    reader = driver.orientation_samples.reader(:type => :buffer, :size => 100)
+    loop do        
+	while sample = reader.read_new
+          print("#{sample.time.to_f} #{sample.orientation.yaw}\r\n")
+	end
+	sleep 0.01
+        Vizkit.process_events
+    end
+end
+
